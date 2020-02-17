4bc1ee83062b7e67ba9eb6f24e2f0c095bed5bb9
imthaghost <imthaghost@protonmail.com>
2020-02-16 21:20:05 -0800

    [update]

--- server.go
+++ server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	//"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
 
 	"github.com/imthaghost/makescraper/controllers"
